aa4df850f98615e960c89a1ccc531b7b777fd0ed
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-03-03 00:41:25 +0300

    add more test cases for isVpnActive

--- test/vpn_detector_method_channel_test.dart
+++ test/vpn_detector_method_channel_test.dart
@@ -3,22 +3,78 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:vpn_detector/vpn_detector_method_channel.dart';
 
 void main() {
-  MethodChannelVpnDetector platform = MethodChannelVpnDetector();
-  const MethodChannel channel = MethodChannel('vpn_detector');
+  late MethodChannelVpnDetector platform;
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return true;
-    });
+    platform = MethodChannelVpnDetector();
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestWidgetsFlutterBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(platform.methodChannel, null);
   });
 
-  test('isVpnActive', () async {
-    expect(await platform.isVpnActive(), true);
+  group('isVpnActive |', () {
+    test(
+      'verify the result of isVpnActive '
+      'when platform channel returns true.',
+      () async {
+        TestWidgetsFlutterBinding.instance.defaultBinaryMessenger
+            .setMockMethodCallHandler(
+          platform.methodChannel,
+          (MethodCall methodCall) {
+            if (methodCall.method == 'isVpnActive') {
+              return Future.value(true);
+            }
+
+            return null;
+          },
+        );
+
+        expect(await platform.isVpnActive(), true);
+      },
+    );
+
+    test(
+      'verify the result of isVpnActive '
+      'when platform channel returns false.',
+      () async {
+        TestWidgetsFlutterBinding.instance.defaultBinaryMessenger
+            .setMockMethodCallHandler(
+          platform.methodChannel,
+          (MethodCall methodCall) {
+            if (methodCall.method == 'isVpnActive') {
+              return Future.value(false);
+            }
+
+            return null;
+          },
+        );
+
+        expect(await platform.isVpnActive(), false);
+      },
+    );
+
+    test(
+      'verify the result of isVpnActive '
+      'when platform channel returns null.',
+      () async {
+        TestWidgetsFlutterBinding.instance.defaultBinaryMessenger
+            .setMockMethodCallHandler(
+          platform.methodChannel,
+          (MethodCall methodCall) {
+            if (methodCall.method == 'isVpnActive') {
+              return Future.value(null);
+            }
+
+            return null;
+          },
+        );
+
+        expect(await platform.isVpnActive(), false);
+      },
+    );
   });
 }
